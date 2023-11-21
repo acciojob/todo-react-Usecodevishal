eb944dff76e2116cfd58c47c08de0c0a38a304bd
vishal bharti <vishconcious112@gmail.com>
2023-11-21 20:30:07 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import Todoinput from "./Todo-input";
 
 const App = () => {
   return (
-    <div>
+    <div id="container">
         {/* Do not remove the main div */}
+        <h1>To-do List</h1>
+        <Todoinput/>
     </div>
   )
 }
---
+++ src/components/Todo-input.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+function Todoinput() {
+  const [todo, setTodo] = useState("");
+  const [todId, setTodoId] = useState();
+
+  const [allTodos, setAllTodos] = useState([]);
+
+  const addTodoHandler = () => {
+    setAllTodos([...allTodos,todo]);
+    console.log(allTodos,todo);
+    setTodo("");
+    
+  };
+
+  const handleDelete = (e) => {
+    setAllTodos(allTodos.filter((el,index) => ( index != e.target.value)))
+  }
+
+  return (
+    <>
+      <input
+        
+        placeholder="Add Todo"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      ></input>
+      <button onClick={addTodoHandler}>Add Todo</button>
+
+      {allTodos.map((el,index) => (
+        
+            <div 
+            key={index}
+            style={{
+                display:"flex",
+                justifyContent:"center",
+                alignItems: "center",
+                gap:"15rem"
+            }}>
+                <h4>{el}</h4>
+                <button 
+                value={index}
+                onClick={handleDelete}
+                style={{
+                    width:"50px",
+                    height:"30px"
+                }}>Delete</button>
+            </div>
+        
+      ))}
+    </>
+  );
+}
+
+export default Todoinput;
